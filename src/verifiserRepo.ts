8b7c0da6f997fbb31fae8f724663d8e04c868674
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2022-05-05 11:18:52 +0200

    Sjekker ikke up to date

--- src/verifiserRepo.ts
+++ src/verifiserRepo.ts
@@ -57,41 +57,80 @@ async function verifiserDefaultBranchProtection(
             branch,
         }
     )
+
+    let ok = true
+
     if (!protection.data.required_status_checks) {
-        throw Error(`${repo} mangler required status checks`)
+        ok = false
+        console.log(`${repo.name} mangler required status checks`)
     }
 
-    if (protection.data.required_status_checks.strict != true) {
-        throw Error(`${repo} har ikke strict status check (branch up to date)`)
+    if (protection.data.required_status_checks?.strict != false) {
+        console.log(`${repo.name} har strict status check (branch up to date)`)
+        ok = false
     }
-    if (!protection.data.required_status_checks.contexts) {
-        throw Error(`${repo} har ikke strict status check (branch up to date)`)
-    }
-    if (protection.data.required_status_checks.contexts.length !== 1) {
-        throw Error(
-            `${repo} har mer enn en status check, vi støtter ikke det i denne koden ennå`
+    if (!protection.data.required_status_checks?.contexts) {
+        console.log(
+            `${repo.name} har ikke strict status check (branch up to date)`
         )
+        ok = false
     }
-    const context = protection.data.required_status_checks.contexts
+
+    const context = protection.data.required_status_checks?.contexts
     for (const check of repo.checks) {
-        if (!context.includes(check)) {
-            throw Error(`${repo.name} har ikke ${check} påkrevd`)
+        if (!context || !context.includes(check)) {
+            console.log(`${repo.name} har ikke ${check} påkrevd`)
+            ok = false
         }
     }
 
     if (!protection.data.required_pull_request_reviews) {
-        throw Error(`${repo} har ikke required_pull_request_reviews`)
+        console.log(`${repo.name} har ikke required_pull_request_reviews`)
+        ok = false
     }
     if (
         protection.data.required_pull_request_reviews
-            .required_approving_review_count != 1
+            ?.required_approving_review_count != 1
     ) {
-        throw Error(`${repo} har required_approving_review_count != 1`)
+        console.log(`${repo.name} har required_approving_review_count != 1`)
+        ok = false
     }
     if (
         protection.data.required_pull_request_reviews
-            .require_code_owner_reviews != true
+            ?.require_code_owner_reviews != true
     ) {
-        throw Error(`${repo} har require_code_owner_reviews != true`)
+        console.log(`${repo.name} har require_code_owner_reviews != true`)
+        ok = false
+    }
+    if (!ok) {
+        console.log(
+            `Oppdaterer branch protection innstillinger for ${repo.name}`
+        )
+        await octokit.request(
+            'PUT /repos/{owner}/{repo}/branches/{branch}/protection',
+            {
+                owner: config.owner,
+                repo: repo.name,
+                branch,
+                required_status_checks: {
+                    strict: false,
+                    contexts: repo.checks,
+                },
+                enforce_admins: false,
+                required_pull_request_reviews: {
+                    dismiss_stale_reviews: false,
+                    require_code_owner_reviews: true,
+                    required_approving_review_count: 1,
+                },
+                restrictions: {
+                    users: [],
+                    teams: [],
+                },
+                required_linear_history: true,
+                allow_force_pushes: false,
+                allow_deletions: false,
+                required_conversation_resolution: true,
+            }
+        )
     }
 }
