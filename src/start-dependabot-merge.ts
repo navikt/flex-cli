7de4f5a619e62525fdd13893fde2351f28219113
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2022-12-29 14:52:45 +0100

    Script for å starte dependabot workflows

---
+++ src/start-dependabot-merge.ts
@@ -0,0 +1,27 @@
+import { config } from './config'
+import { octokit } from './octokit'
+
+console.log('\n\nStarter dependabot i alle repoer ')
+
+for (const r of config.repos) {
+    try {
+        await octokit.request(
+            'POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches',
+            {
+                owner: config.owner,
+                repo: r.name,
+                workflow_id: 'merge-dependabot-pr.yml',
+                ref: 'master',
+            }
+        )
+        console.log(`Startet workflow i ${r.name}`)
+    } catch (e: any) {
+        if (e.status != 404) {
+            console.log('Kunne ikke starte workflow i ' + r.name, e)
+        } else {
+            console.log(`Startet ikke workflow i ${r.name}`)
+        }
+    }
+}
+
+console.log('\n\nAlt startet')
