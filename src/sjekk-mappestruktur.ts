55ee14524061ce91431df4e53653d5a6984d26fb
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2022-12-29 11:25:08 +0100

    Mappestruktur sjekk

---
+++ src/sjekk-mappestruktur.ts
@@ -0,0 +1,33 @@
+import { config } from './config'
+
+import * as fs from 'fs'
+
+import * as path from 'path'
+
+console.log('\n\nSjekker mappen over for alle repoer ')
+
+const files = await fs.promises.readdir('./..')
+const directories = files.filter((f) =>
+    fs.statSync(path.join('./..', f)).isDirectory()
+)
+const kjenteRepoer = config.repos.map((r) => r.name)
+
+const manglerFraConfig = directories.filter((x) => !kjenteRepoer.includes(x))
+
+if (manglerFraConfig.length > 0) {
+    console.log('Følgende repoer mangler i config.yml:')
+    console.log(manglerFraConfig)
+} else {
+    console.log('Ingen repoer mangler i config.yml')
+}
+
+const manglerFraDisk = kjenteRepoer.filter((x) => !directories.includes(x))
+
+if (manglerFraDisk.length > 0) {
+    console.log('Følgende repoer mangler fra disk')
+    console.log(manglerFraDisk)
+} else {
+    console.log('Ingen repoer mangler fra disk')
+}
+
+console.log('\n\nAlt patchet')
