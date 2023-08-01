519e92d2556c7baf32fb6048d1ee51923c52fd9c
kulsveen <bjorn.inge.kulsveen@nav.no>
2023-08-01 15:59:38 +0200

    Klon alle repo med Github CLI

--- src/klon-alle.ts
+++ src/klon-alle.ts
@@ -1,18 +1,36 @@
 import { config } from './config'
 import { execSync } from 'node:child_process'
+import * as prompts from 'prompts'
 
 console.log('\n\nKloner flex repoer til parent mappen ')
 const eksisterende = execSync('ls ..').toString()
+
+const response = await prompts([
+    {
+        type: 'select',
+        name: 'git',
+        message: 'Bruker du git eller Github CLI (gh)',
+        choices: [{ title: 'git', value: 'git' }, { title: 'Github CLI', value: 'gh' }],
+    },
+])
 
 for (const r of config.repos) {
     if (eksisterende.includes(r.name)) {
         console.log(`${r.name} eksisterer allerede`)
         continue
     }
-    const res = execSync(`git clone git@github.com:navikt/${r.name}.git`, {
-        cwd: '..',
-    })
-    console.log(res.toString())
+    if (response.git === 'git') {
+        const res = execSync(`git clone git@github.com:navikt/${r.name}.git`, {
+            cwd: '..',
+        })
+        console.log(res.toString())
+    }
+    if (response.git === 'gh') {
+        const res = execSync(`gh repo clone navikt/${r.name}`, {
+            cwd: '..',
+        })
+        console.log(res.toString())
+    }
 }
 
 console.log('\n\nAlt klonet')
