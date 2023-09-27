4dca3139f4ba7fd812d5c450c2fcb640ca14cb09
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2023-09-27 19:12:16 +0200

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers'
 import { klonAlle } from './klon-alle.ts'
+import { startDependabotMerge } from './start-dependabot-merge.ts'
+import { verifiserRepoer } from './verifiser.ts'
 
 await yargs(hideBin(process.argv))
     .scriptName('npm start')
@@ -9,6 +11,16 @@ await yargs(hideBin(process.argv))
         'Kloner alle repos i teamet',
 
         async () => await klonAlle()
+    )
+    .command(
+        'start-auto-merge',
+        'Starter dependabot automerging i alle repoer',
+        async () => await startDependabotMerge()
+    )
+    .command(
+        'verifiser-repoer',
+        'Verifiserer oppsettet i alle repoer',
+        async () => await verifiserRepoer()
     )
     .demandCommand()
     .strict()
