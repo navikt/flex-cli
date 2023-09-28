72cdde8429437765614bdd776c139b6dfa73f690
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2023-09-28 11:35:02 +0200

    bcp

--- src/actions/branch-commit-push.ts
+++ src/actions/branch-commit-push.ts
@@ -5,128 +5,130 @@ import * as prompts from 'prompts'
 import { config } from '../config/config'
 import { log } from '../common/log.ts'
 
-const commit = await prompts([
-    {
-        type: 'text',
-        name: 'melding',
-        message: 'Hvilken commit melding vil du gi?',
-    },
-])
+export async function branchCommitPush() {
+    const commit = await prompts([
+        {
+            type: 'text',
+            name: 'melding',
+            message: 'Hvilken commit melding vil du gi?',
+        },
+    ])
 
-if (!commit.melding) {
-    process.exit(1)
-}
-const branchNavn = await prompts([
-    {
-        type: 'text',
-        name: 'branch',
-        message: 'Hva skal branchnavnet være?',
-    },
-])
+    if (!commit.melding) {
+        process.exit(1)
+    }
+    const branchNavn = await prompts([
+        {
+            type: 'text',
+            name: 'branch',
+            message: 'Hva skal branchnavnet være?',
+        },
+    ])
 
-if (!branchNavn.branch) {
-    process.exit(1)
-}
+    if (!branchNavn.branch) {
+        process.exit(1)
+    }
 
-const repoerMedEndringer: string[] = []
-for (const r of config.repos) {
-    if (r.name == 'flex-cli') {
-        // skipper dette repoet
-    } else {
-        let endringer = false
+    const repoerMedEndringer: string[] = []
+    for (const r of config.repos) {
+        if (r.name == 'flex-cli') {
+            // skipper dette repoet
+        } else {
+            let endringer = false
+            try {
+                execSync('git diff-index --quiet HEAD', {
+                    cwd: `../${r.name}`,
+                })
+            } catch (e: any) {
+                endringer = true
+            }
+            if (endringer) {
+                log('Fant endringer i ' + r.name)
+                execSync(`git checkout -b ${branchNavn.branch}`, {
+                    cwd: `../${r.name}`,
+                })
+                execSync('git add .', {
+                    cwd: `../${r.name}`,
+                })
+                execSync(`git commit -m "${commit.melding}"`, {
+                    cwd: `../${r.name}`,
+                })
+                execSync('git push', {
+                    cwd: `../${r.name}`,
+                })
+                repoerMedEndringer.push(r.name)
+            } else {
+                log('Fant ingen endringer i ' + r.name)
+            }
+        }
+    }
+
+    if (repoerMedEndringer.length == 0) {
+        log('Ingen endringer å lage PR for')
+        process.exit(1)
+    }
+
+    const lagPr = await prompts([
+        {
+            type: 'confirm',
+            name: 'ok',
+            message: `Vil du lage PR for endringene i ${repoerMedEndringer.join(', ')}?`,
+        },
+    ])
+
+    if (!lagPr.ok) {
+        process.exit(1)
+    }
+
+    async function sleep(ms: number) {
+        return new Promise((resolve) => setTimeout(resolve, ms))
+    }
+
+    async function lagPR(repo: string) {
         try {
-            execSync('git diff-index --quiet HEAD', {
-                cwd: `../${r.name}`,
+            log('Lager PR for ' + repo)
+            execSync(`gh pr create --title "${commit.melding}" --body "Fra flex-cli"`, {
+                cwd: `../${repo}`,
             })
         } catch (e: any) {
-            endringer = true
-        }
-        if (endringer) {
-            log('Fant endringer i ' + r.name)
-            execSync(`git checkout -b ${branchNavn.branch}`, {
-                cwd: `../${r.name}`,
-            })
-            execSync('git add .', {
-                cwd: `../${r.name}`,
-            })
-            execSync(`git commit -m "${commit.melding}"`, {
-                cwd: `../${r.name}`,
-            })
-            execSync('git push', {
-                cwd: `../${r.name}`,
-            })
-            repoerMedEndringer.push(r.name)
-        } else {
-            log('Fant ingen endringer i ' + r.name)
+            log('retry om 10 sekunder')
+            await sleep(10000)
+            await lagPR(repo)
         }
     }
-}
 
-if (repoerMedEndringer.length == 0) {
-    log('Ingen endringer å lage PR for')
-    process.exit(1)
-}
-
-const lagPr = await prompts([
-    {
-        type: 'confirm',
-        name: 'ok',
-        message: `Vil du lage PR for endringene i ${repoerMedEndringer.join(', ')}?`,
-    },
-])
-
-if (!lagPr.ok) {
-    process.exit(1)
-}
-
-async function sleep(ms: number) {
-    return new Promise((resolve) => setTimeout(resolve, ms))
-}
-
-async function lagPR(repo: string) {
-    try {
-        log('Lager PR for ' + repo)
-        execSync(`gh pr create --title "${commit.melding}" --body "Fra flex-cli"`, {
-            cwd: `../${repo}`,
-        })
-    } catch (e: any) {
-        log('retry om 10 sekunder')
-        await sleep(10000)
-        await lagPR(repo)
+    for (const r of repoerMedEndringer) {
+        await lagPR(r)
     }
-}
 
-for (const r of repoerMedEndringer) {
-    await lagPR(r)
-}
+    const automerge = await prompts([
+        {
+            type: 'confirm',
+            name: 'ok',
+            message: `Vil du automerge endringene i ${repoerMedEndringer.join(
+                ', ',
+            )} til master slik at det går i produksjon?`,
+        },
+    ])
 
-const automerge = await prompts([
-    {
-        type: 'confirm',
-        name: 'ok',
-        message: `Vil du automerge endringene i ${repoerMedEndringer.join(
-            ', ',
-        )} til master slik at det går i produksjon?`,
-    },
-])
+    if (!automerge.ok) {
+        process.exit(1)
+    }
 
-if (!automerge.ok) {
-    process.exit(1)
-}
+    async function automergePr(r: string) {
+        try {
+            log('Automerger PR for ' + r)
+            execSync('gh pr merge --auto -s', {
+                cwd: `../${r}`,
+            })
+        } catch (e: any) {
+            log('retry om 10 sekunder')
+            await sleep(10000)
+            await automergePr(r)
+        }
+    }
 
-async function automergePr(r: string) {
-    try {
-        log('Automerger PR for ' + r)
-        execSync('gh pr merge --auto -s', {
-            cwd: `../${r}`,
-        })
-    } catch (e: any) {
-        log('retry om 10 sekunder')
-        await sleep(10000)
+    for (const r of repoerMedEndringer) {
         await automergePr(r)
     }
-}
-
-for (const r of repoerMedEndringer) {
-    await automergePr(r)
 }
--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import { startDependabotMerge } from './actions/start-dependabot-merge.ts'
 import { verifiserRepoer } from './actions/verifiser.ts'
 import { patchAlle } from './actions/patch-alle.ts'
 import { resetAltTilMaster } from './actions/reset-alt-til-master.ts'
+import { branchCommitPush } from './actions/branch-commit-push.ts'
 
 await yargs(hideBin(process.argv))
     .scriptName('npm start')
@@ -20,6 +21,7 @@ await yargs(hideBin(process.argv))
         'Starter dependabot automerging i alle repoer',
         async () => await startDependabotMerge(),
     )
+    .command('bcp', 'Brancher, committer, pusher og lager pullrequest for', async () => await branchCommitPush())
     .command('patch-repoer', 'Verifiserer og patcher oppsettet i alle repoer', async () => await patchAlle())
     .command('verifiser-repoer', 'Verifiserer oppsettet i alle repoer', async () => await verifiserRepoer())
     .command('reset-master', 'Resetter alle repoer til master', async () => await resetAltTilMaster())
