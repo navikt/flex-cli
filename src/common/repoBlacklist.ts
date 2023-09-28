6060e61a1e1df288bef6b5d3954b387630a14fe0
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2023-09-28 12:44:49 +0200

    Oppdater repo blacklist

--- src/common/repoBlacklist.ts
+++ src/common/repoBlacklist.ts
@@ -1,4 +1,11 @@
-const blacklist: string[] = ['vault-iac']
+const blacklist: string[] = [
+    'vault-iac',
+    'nav-dekoratoren-server-component',
+    'next-logger',
+    'next-api-proxy',
+    'next-auth-wonderwall',
+    'aad-iac',
+]
 
 export function blacklisted<Repo extends { name: string }>(repo: Repo): boolean {
     return !blacklist.includes(repo.name)
